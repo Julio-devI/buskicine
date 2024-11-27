c997a8bec01ab151bcd6aee9fac315e89631befa
julio-DevI <julio.a.valenca@gmail.com>
2024-11-27 08:42:42 -0300

    feat: registro de usuario

--- src/main/java/com/example/projetoptwo/controllers/RegisterController.java
+++ src/main/java/com/example/projetoptwo/controllers/RegisterController.java
@@ -7,6 +7,8 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.*;
+
 public class RegisterController {
     @FXML
     private TextField emailField;
@@ -21,6 +23,8 @@ public class RegisterController {
     private Button registerButton, backToLoginButton;
 
     private Stage stage;
+
+    private static final String path = "/com/example/projetoptwo/database/users.txt";
 
     public void initialize() {
         registerButton.setOnAction(e -> handleSignUp());
@@ -51,7 +55,45 @@ public class RegisterController {
             return;
         }
 
-        showAlert("Sucesso", "Cadastro realizado com sucesso");
+        try {
+            if (UserAlreadyExists(email)) {
+                showAlert("Erro", "Este e-mail já está cadastrado");
+                return;
+            }
+
+            saveUser(email, password);
+            showAlert("Sucesso", "Cadastro realizado com sucesso");
+
+            emailField.clear();
+            passwordField.clear();
+            confirmPasswordField.clear();
+        } catch (IOException e) {
+            showAlert("Erro", "Não foi possível salvar o cadastro" + e.getMessage());
+        }
+    }
+
+    private boolean UserAlreadyExists(String email) throws IOException {
+        try (BufferedReader leitor = new BufferedReader(new FileReader(path))) {
+            String linha;
+            while ((linha = leitor.readLine()) != null) {
+                // Dividir a linha em email e senha
+                String[] partes = linha.split(";");
+                if (partes.length > 0 && partes[0].equalsIgnoreCase(email)) {
+                    return true; // Usuário encontrado
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        }
+        return false;
+    }
+
+    private void saveUser(String email, String password) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(path, true)))
+        {
+            writer.write(email + ";" + password);
+            writer.newLine();
+        }
     }
 
     private void navigateToLogin() {
